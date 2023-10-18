16003929a6352396f4ad3ec43db37b0926551ec1
TAJLNsScripts <138971402+TAJLNsScripts@users.noreply.github.com>
2023-10-18 22:46:37 +0200

    Switched to dash, added drm support

--- ufc.py
+++ ufc.py
@@ -4,6 +4,13 @@ import json
 import base64
 import m3u8
 import os
+from pywidevine.cdm import Cdm
+from pywidevine.device import Device
+from pywidevine.pssh import PSSH
+from bs4 import BeautifulSoup
+
+#Config
+PATH_TO_WVD = './WVD.wvd'
 
 def ascii_clear():
     os.system('cls||clear')
@@ -29,6 +36,48 @@ def ascii_clear():
                                     TAJLN 2023
 """)
 
+def do_cdm(pssh, license_url, licence_token, key_id):
+    pssh = PSSH(pssh)
+
+    device = Device.load(PATH_TO_WVD)
+    cdm = Cdm.from_device(device)
+    session_id = cdm.open()
+    challenge = cdm.get_license_challenge(session_id, pssh)
+
+    drm_info_json = {
+        "system":"com.widevine.alpha",
+        "key_ids":[key_id]
+    }
+
+    headers = {
+            'Authorization': 'Bearer ' + licence_token,
+            'X-DRM-INFO': base64.b64encode(json.dumps(drm_info_json).encode()).decode(),
+            'Content-Type': 'application/octet-stream',
+            'User-Agent': 'Dice Shield/12.11.0 (Linux;Android 9) ExoPlayerLib/2.18.4',
+    }
+
+    licence = requests.post(license_url, headers=headers, data=challenge)
+    licence.raise_for_status()
+
+    cdm.parse_license(session_id, licence.content)
+
+    keys = cdm.get_keys(session_id)
+    
+    cdm.close(session_id)
+
+    for key in keys:
+        if key.type != 'SIGNING':
+            return f"{key.kid.hex}:{key.key.hex()}"
+
+def find_wv_pssh_offset(raw: bytes) -> str:
+    offset = raw.rfind(b'pssh')
+    return raw[offset - 4:offset - 4 + raw[offset - 1]]
+
+
+def to_pssh(content: bytes) -> str:
+    wv_offset = find_wv_pssh_offset(content)
+    return base64.b64encode(wv_offset).decode()
+
 def current_milli_time():
     return round(time.time() * 1000)
 
@@ -51,7 +100,7 @@ def refresh_token(token):
     }
     
     post_data = {
-        "refreshToken": "eyJhbGciOiJSUzI1NiIsInB1ciI6IlJFRiIsInNpZyI6ImciLCJ0eXAiOiJKV1QiLCJ2IjozfQ.eyJhcCI6eyJhcHQiOiJJRCJ9LCJhcHIiOiJJRCIsImF1ZCI6ImRjZS51ZmMiLCJkZXYiOiJBTkRST0lEX1BIT05FIiwiZW52IjoicHJvZCIsImV4cCI6MTY5NjkwODg2NCwiZ3VlIjpmYWxzZSwiaWF0IjoxNjg4MjY4ODY0LCJpc3MiOiJkY2UtaWQiLCJzdWIiOiJWY29QdW58NjAzYjVhNDAtNDU0MC00ZjdjLTgzNGMtOTU5ZGI4Y2Y2M2MyIn0.WXIE3Wy_EnUwux146leJtNYN5mrK9yDQw7WEWQGwb9ASGv7WWkLgKNNbqJ9sSCaNB6S_YYUkusf9AdA-dnUlDgfKYVMtBW-t5dTHsRHuSDxkuE_5-jsRu0MqdP5_S031-j9sfXSDL644_lz35PdGlXqGgK5kIicrj5cU5SEyNRs"
+        "refreshToken": "eyJhbGciOiJSUzI1NiIsInB1ciI6IlJFRiIsInNpZyI6ImciLCJ0eXAiOiJKV1QiLCJ2IjozfQ.eyJhcCI6eyJhcHQiOiJJRCJ9LCJhcHIiOiJJRCIsImF1ZCI6ImRjZS51ZmMiLCJkZXYiOiJBTkRST0lEX1BIT05FIiwiZW52IjoicHJvZCIsImV4cCI6MTY5OTMyNDQ1OCwiZ3VlIjpmYWxzZSwiaWF0IjoxNjkwNjg0NDU4LCJpc3MiOiJkY2UtaWQiLCJzdWIiOiJhWHZLclp8MjQ2YWRiNGEtOGI4YS00NWYwLWI2NGYtYjg1ODgxODUzYjNmIn0.DHydVhuWBNdIvfubRG9XujU8IDycfGo3GRdsVpyNArR-2t-WzKKrbD4K-EtYWB6kSAMGtEnfZ8-uYUSxTmBIM0vK3MV8_le5zd5jLCzMoWM89CkwaGWHLkjSMnlrxYagzPeWhLtYZplg7rxygYCh6uX7VG76WVAohmUqaLOZPro"
     }
     
     post_data = json.dumps(post_data)
@@ -148,7 +197,6 @@ def spoof_buy(token, androidIAPCodes):
     purchase_token = 'bfokzzrsaqtxcmwklgnxwztz.AO-J1OsFfDYXhRrvKGFBqjOUCiamHLDdHEetbxCVAoBNpOD-mTIQtoB_lLPEinJmPUMurynpiPutfRNNgHlcekcnGpWZiFsSGRYTHOveFjrKRgeLKDYcevlhZbWnRXOLhOGMjKztehMT'
     purchase_time = str(current_milli_time())
     
-    #reciept_string = '{"orderId":"6660823415274633304.5480098017970283","packageName":"com.neulion.smartphone.ufc.android","productId":"' + androidIAPCodes + '","purchaseTime":' + str(current_milli_time()) + ',"purchaseState":0,"purchaseToken":"bfokzzrsaqtxcmwklgnxwztz.AO-J1OsFfDYXhRrvKGFBqjOUCiamHLDdHEetbxCVAoBNpOD-mTIQtoB_lLPEinJmPUMurynpiPutfRNNgHlcekcnGpWZiFsSGRYTHOveFjrKRgeLKDYcevlhZbWnRXOLhOGMjKztehMT","developerPayload":"subs:' + androidIAPCodes + ':{\\"purchaseStrategy\\":{\\"type\\":\\"SUBSCRIPTION\\",\\"subscriptionPeriod\\":\\"P1M\\",\\"subscriptionMarketingPricePeriod\\":\\"P1M\\"},\\"id\\":168}","receiptData":"{\\"orderId\\":\\"5560823415974633704.5480098017970283\\",\\"packageName\\":\\"com.neulion.smartphone.ufc.android\\",\\"productId\\":\\"' + androidIAPCodes + '\\",\\"purchaseTime\\":1688075795889,\\"purchaseState\\":0,\\"purchaseToken\\":\\"bfokzzrsaqtxcmwklgnxwztz.AO-J1OsFfDYXhRrvKGFBqjOUCiamHLDdHEetbxCVAoBNpOD-mTIQtoB_lLPEinJmPUMurynpiPutfRNNgHlcekcnGpWZiFsSGRYTHOveFjrKRgeLKDYcevlhZbWnRXOLhOGMjKztehMT\\",\\"developerPayload\\":\\"subs:' + androidIAPCodes + ':{\\\\\\"purchaseStrategy\\\\\\":{\\\\\\"type\\\\\\":\\\\\\"SUBSCRIPTION\\\\\\",\\\\\\"subscriptionPeriod\\\\\\":\\\\\\"P1M\\\\\\",\\\\\\"subscriptionMarketingPricePeriod\\\\\\":\\\\\\"P1M\\\\\\"},\\\\\\"id\\\\\\":168}\\"}"}'
     reciept_string = '{"orderId":"' + order_id + '","packageName":"com.neulion.smartphone.ufc.android","productId":"' + androidIAPCodes + '","purchaseTime":' + purchase_time + ',"purchaseState":0,"purchaseToken":"' + purchase_token + '","developerPayload":"subs:' + androidIAPCodes + ':{\\"purchaseStrategy\\":{\\"type\\":\\"SUBSCRIPTION\\",\\"subscriptionPeriod\\":\\"P1M\\",\\"subscriptionMarketingPricePeriod\\":\\"P1M\\"},\\"id\\":168}","receiptData":"{\\"orderId\\":\\"6660823415974633704.5480098017970283\\",\\"packageName\\":\\"com.neulion.smartphone.ufc.android\\",\\"productId\\":\\"' + androidIAPCodes + '\\",\\"purchaseTime\\":' + purchase_time + ',\\"purchaseState\\":0,\\"purchaseToken\\":\\"' + purchase_token + '\\",\\"developerPayload\\":\\"subs:' + androidIAPCodes + ':{\\\\\\"purchaseStrategy\\\\\\":{\\\\\\"type\\\\\\":\\\\\\"SUBSCRIPTION\\\\\\",\\\\\\"subscriptionPeriod\\\\\\":\\\\\\"P1M\\\\\\",\\\\\\"subscriptionMarketingPricePeriod\\\\\\":\\\\\\"P1M\\\\\\"},\\\\\\"id\\\\\\":168}\\"}"}'
     reciept_encoded = base64.b64encode(reciept_string.encode()).decode()
     
@@ -213,7 +261,7 @@ def request_stream(url):
     if response.status_code == 200:
         print('Stream request successful')
         data = response.json()
-        return data['hlsUrl']
+        return [data['hls']['url'], data['dash']['url'], data['dash']['drm']['url'], data['dash']['drm']['jwtToken']]
     else:
         print('Stream request failed')
         print(response.content)
@@ -258,11 +306,37 @@ def stream_exteract(url, user_agent):
     except Exception as e:
         print('Stream extraction failed')
         print(e)
+          
+def get_pssh_perm_key_id(dash_url, user_agent):
+    headers = {
+       'User-Agent': user_agent
+    }
     
+    response = requests.get(dash_url, headers=headers, allow_redirects=False)
+    location_url = response.headers['location']
     
-ascii_clear()    
+    response = requests.get(location_url, headers=headers)
     
-token = 'eyJhbGciOiJSUzI1NiIsInB1ciI6IkFVVCIsInNpZyI6ImciLCJ0eXAiOiJKV1QiLCJ2IjozfQ.eyJhcCI6eyJhcHQiOiJJRCJ9LCJhcHIiOiJJRCIsImF1ZCI6WyJkY2UudWZjIl0sImRldiI6IkFORFJPSURfUEhPTkUiLCJlbnYiOiJwcm9kIiwiZXhwIjoxNjg4MjY5NDY0LCJndWUiOmZhbHNlLCJpYXQiOjE2ODgyNjg4NjQsImlwIjoiMTQ5LjcuMTYuMTQ1IiwiaXNzIjoiZGNlLWlkIiwibG8yIjoiR0IsRW5nbGFuZCxFbmdsYW5kLExvbmRvbixFQzRSLDAsMSwwIiwicm9sIjoiQ1VTVE9NRVIiLCJzdWIiOiJWY29QdW58NjAzYjVhNDAtNDU0MC00ZjdjLTgzNGMtOTU5ZGI4Y2Y2M2MyIiwidXRwIjoiSFVNQU4ifQ.TdtxoCGagJbR6s_-PYsHteZVeizk0Ydbol9CZ5POA_stIGeuIrZGU9a99zMWgrpoVZ5L8a-xVbuVg-1G6wu6B4ub5kZToAzDHVG77CpuK8nI5jWGXwG0OqYvoFU398VDUEUpAzGBv_wnetPQEyNnxcaDJF_7sYhmPYrJGgWklNk'
+    soup = BeautifulSoup(response.content, features="xml")
+    audio_repr = soup.findAll('Representation')[-1]
+    audio_base_url = audio_repr.find('BaseURL').text
+    audio_key_id = audio_repr.find('ContentProtection')['cenc:default_KID']
+    
+    location_parts = location_url.split('/')
+    location_parts.pop()
+    
+    init_url = '/'.join(location_parts) + '/' + audio_base_url + 'init_' + audio_repr['id'] + '.m4s'
+    
+    init_response = requests.get(init_url, headers=headers)
+    
+    return [to_pssh(init_response.content), location_url, audio_key_id]
+    
+ascii_clear()
+#token = 'eyJhbGciOiJSUzI1NiIsInB1ciI6IkFVVCIsInNpZyI6ImciLCJ0eXAiOiJKV1QiLCJ2IjozfQ.eyJhcCI6eyJhcHQiOiJJRCJ9LCJhcHIiOiJJRCIsImF1ZCI6WyJkY2UudWZjIl0sImRldiI6IkFORFJPSURfUEhPTkUiLCJlbnYiOiJwcm9kIiwiZXhwIjoxNjg4MjY5NDY0LCJndWUiOmZhbHNlLCJpYXQiOjE2ODgyNjg4NjQsImlwIjoiMTQ5LjcuMTYuMTQ1IiwiaXNzIjoiZGNlLWlkIiwibG8yIjoiR0IsRW5nbGFuZCxFbmdsYW5kLExvbmRvbixFQzRSLDAsMSwwIiwicm9sIjoiQ1VTVE9NRVIiLCJzdWIiOiJWY29QdW58NjAzYjVhNDAtNDU0MC00ZjdjLTgzNGMtOTU5ZGI4Y2Y2M2MyIiwidXRwIjoiSFVNQU4ifQ.TdtxoCGagJbR6s_-PYsHteZVeizk0Ydbol9CZ5POA_stIGeuIrZGU9a99zMWgrpoVZ5L8a-xVbuVg-1G6wu6B4ub5kZToAzDHVG77CpuK8nI5jWGXwG0OqYvoFU398VDUEUpAzGBv_wnetPQEyNnxcaDJF_7sYhmPYrJGgWklNk'
+token = 'eyJhbGciOiJSUzI1NiIsInB1ciI6IkFVVCIsInNpZyI6ImciLCJ0eXAiOiJKV1QiLCJ2IjozfQ.eyJhcCI6eyJhcHQiOiJJRCJ9LCJhcHIiOiJJRCIsImF1ZCI6WyJkY2UudWZjIl0sImRldiI6IkFORFJPSURfUEhPTkUiLCJlbnYiOiJwcm9kIiwiZXhwIjoxNjkwNjg1MDU4LCJndWUiOmZhbHNlLCJpYXQiOjE2OTA2ODQ0NTgsImlwIjoiMTQ2LjcwLjExNi4xODIiLCJpc3MiOiJkY2UtaWQiLCJsbzIiOiJBVCxWaWVubmEsVmllbm5hLFZpZW5uYSwxMjMwLDAsMSwwIiwicm9sIjoiQ1VTVE9NRVIiLCJzdWIiOiJhWHZLclp8MjQ2YWRiNGEtOGI4YS00NWYwLWI2NGYtYjg1ODgxODUzYjNmIiwidXRwIjoiSFVNQU4ifQ.c3fg5QpwJ3s9YB7R8Fd-UE59_N306MgG0S8vHOKdgD8EzNCyuflDTI-B8JgeYOlyfHN07KivjbZ9DFJwD8B9HRwalSP39hfCRUO00uqPbBZapys_zq0MEtG0SLiqJULewZEICm2Kv6mHR-OjtUy_PBmb9gv_ZWDUjWpNf1fN-Zw'
+
+#user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36'
+user_agent = 'ExoDoris/12.11.0 (Linux;Android 9) ExoPlayerLib/2.18.4'
 
 token = refresh_token(token)
 
@@ -276,15 +350,28 @@ if androidIAPCodes is not None:
 
 stream_request_url = request_event(token, event_id)
 
-temp_m3u8 = request_stream(stream_request_url)
+hls_url, dash_url, licence_url, licence_token = request_stream(stream_request_url)
 
-user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36'
-m3u8_url = stream_exteract(temp_m3u8, user_agent)
+pssh, perm_dash, key_id = get_pssh_perm_key_id(dash_url, user_agent)
+
+key = do_cdm(pssh, licence_url, licence_token, key_id)
+print('Retrieved key: ' + key)
+
+fkeys = key.split(":")
+ekeys = base64.b64encode(('{"' + fkeys[0] + '":"' + fkeys[1] + '"}').encode('ascii'))
+print("\nReproductor M3U8 link: " + perm_dash + "?&ck=" + ekeys.decode('ascii'))   
+
+confirm = input('\nEnter Y to save to playlist.m3u: ')
+
+if confirm.lower() != 'y':
+    quit()
 
 f = open("playlist.m3u", "w+")
 f.write('#EXTINF:-1 tvg-logo="https://www.sportsvideo.org/wp-content/uploads/2021/02/ufc-fight-pass-logo.png" group-title="PPV",' + event_name)
 f.write('\n#EXTVLCOPT:http-user-agent=' + user_agent)
-f.write('\n' + m3u8_url)
+f.write('\n#KODIPROP:inputstream.adaptive.license_type=clearkey')
+f.write('\n#KODIPROP:inputstream.adaptive.license_key=' + key)
+f.write('\n' + perm_dash)
 
 ascii_clear()
 
